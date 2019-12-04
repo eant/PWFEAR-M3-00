6d65f6bf5c27923c803302598f6644786c0b3d7b
smessina <silvio.messina@eant.tech>
2019-12-03 21:39:38 -0300

    Docs v2

---
+++ docs/precache-manifest.ec32e01682a7e0d3e0eb07fbaca91c8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e901eb25c6db0fa86474e3af24e44764",
+    "url": "/PWFEAR-M3-00/index.html"
+  },
+  {
+    "revision": "73400613e09ba0463159",
+    "url": "/PWFEAR-M3-00/static/css/main.20509385.chunk.css"
+  },
+  {
+    "revision": "f2fafa1b9dd9797e0920",
+    "url": "/PWFEAR-M3-00/static/js/2.f0368ba5.chunk.js"
+  },
+  {
+    "revision": "73400613e09ba0463159",
+    "url": "/PWFEAR-M3-00/static/js/main.e9dff109.chunk.js"
+  },
+  {
+    "revision": "3aeefe78f7b6704f2445",
+    "url": "/PWFEAR-M3-00/static/js/runtime-main.08327e05.js"
+  }
+]);
