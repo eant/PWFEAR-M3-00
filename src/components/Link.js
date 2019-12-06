2caab5462fb8be6afb91d09e27e2d24cd2e49c13
smessina <silvio.messina@eant.tech>
2019-12-05 22:04:47 -0300

    implementando Reactstrap

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React , { Component } from 'react'
+import { Button } from 'reactstrap';
+
 import './Link.css'
 
-class Link extends React.Component {
+class Link extends Component {
 
   render(){
-    return <a className="App-link" href={this.props.url} target={this.props.blank ? "_blank" : "_self"} rel="noopener noreferrer">{this.props.text}</a>
+    return <Button className="mx-3" color="success">{this.props.text}</Button>
+    /*return <a className="App-link" href={this.props.url} target={this.props.blank ? "_blank" : "_self"} rel="noopener noreferrer">{this.props.text}</a>*/
   }
 
 }
